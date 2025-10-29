5244423c46e6b896952fd27b299beb904a3db0f8
devmani <devmani1456@gmail.com>
2025-10-29 12:12:39 +0545

    bootstrap

--- header.php
+++ header.php
@@ -21,4 +21,10 @@
     // body_class(); 
     ?>
     <?php wp_body_open(); ?>
+    
+    <?php 
+    if(function_exists('wp_body_open')){
+        wp_body_open();
+    }
+    ?>
     <header>Header</header>
